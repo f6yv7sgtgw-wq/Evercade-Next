c5fb6dce214c9985ab3efbf1742b47df286258f5
f6yv7sgtgw-wq <f6yv7sgtgw@privaterelay.appleid.com>
2026-08-07 00:39:05 +0200

    Add central runtime configuration

---
+++ src/config.js
@@ -0,0 +1,8 @@
+window.EVERCADE_CONFIG = Object.freeze({
+  versionFile: 'VERSION.json',
+  storageKey: 'project-evercade-next-v1',
+  pagesUrl: 'https://f6yv7sgtgw-wq.github.io/Evercade-Next/',
+  genericParserWorkerUrl: 'https://genericparser.f6yv7sgtgw.workers.dev',
+  genericParserContract: 'generic-parser-module-v1',
+  dealApiUrl: 'https://project-evercade-deal-api.jnldc.chatgpt.site'
+});
