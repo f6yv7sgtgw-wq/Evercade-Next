771324ac96941ddf600b1e1c303e19006ce62d8e
f6yv7sgtgw-wq <f6yv7sgtgw@privaterelay.appleid.com>
2026-08-07 05:00:19 +0200

    Phase 3: centralize parser endpoints and source URLs

--- src/config.js
+++ src/config.js
@@ -4,5 +4,20 @@ window.EVERCADE_CONFIG = Object.freeze({
   pagesUrl: 'https://f6yv7sgtgw-wq.github.io/Evercade-Next/',
   genericParserWorkerUrl: 'https://genericparser.f6yv7sgtgw.workers.dev',
   genericParserContract: 'generic-parser-module-v1',
-  dealApiUrl: 'https://project-evercade-deal-api.jnldc.chatgpt.site'
+  genericParserSearchPaths: Object.freeze([
+    '/api/module/search',
+    '/api/search',
+    '/search'
+  ]),
+  dealApiUrl: 'https://project-evercade-deal-api.jnldc.chatgpt.site',
+  directSources: Object.freeze([
+    { name: 'eBay Deutschland', url: 'https://www.ebay.de/sch/i.html?_nkw={query}' },
+    { name: 'Kleinanzeigen', url: 'https://www.kleinanzeigen.de/s-{query}/k0' },
+    { name: 'Amazon Deutschland', url: 'https://www.amazon.de/s?k={query}' },
+    { name: 'Google Shopping', url: 'https://www.google.com/search?tbm=shop&q={query}' },
+    { name: 'Idealo', url: 'https://www.idealo.de/preisvergleich/MainSearchProductCategory.html?q={query}' },
+    { name: 'Kaufland', url: 'https://www.kaufland.de/s/?search_value={query}' },
+    { name: 'Retroplace', url: 'https://www.retroplace.com/de/suche?q={query}' },
+    { name: 'DragonBox', url: 'https://dragonbox.de/de/suche?sSearch={query}' }
+  ])
 });
